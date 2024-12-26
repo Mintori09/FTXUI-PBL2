e6ee23925ef474f802256b98df1ba2f0f76e0a81
Tran Van Minh Tri <tvmthtr@gmail.com>
2024-12-26 23:24:49 +0700

    update

---
+++ build/CMakeFiles/ftxui_starter.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mintori/Documents/FTXUI-PBL2/main.cpp" "CMakeFiles/ftxui_starter.dir/main.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/main.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/Account.cpp" "CMakeFiles/ftxui_starter.dir/src/Account.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/Account.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/Accounts.cpp" "CMakeFiles/ftxui_starter.dir/src/Accounts.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/Accounts.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/Movie.cpp" "CMakeFiles/ftxui_starter.dir/src/Movie.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/Movie.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/Movies.cpp" "CMakeFiles/ftxui_starter.dir/src/Movies.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/Movies.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/ShowTime.cpp" "CMakeFiles/ftxui_starter.dir/src/ShowTime.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/ShowTime.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/Showtimes.cpp" "CMakeFiles/ftxui_starter.dir/src/Showtimes.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/Showtimes.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/Ticket.cpp" "CMakeFiles/ftxui_starter.dir/src/Ticket.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/Ticket.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/Tickets.cpp" "CMakeFiles/ftxui_starter.dir/src/Tickets.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/Tickets.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/src/my_lib.cpp" "CMakeFiles/ftxui_starter.dir/src/my_lib.cpp.o" "gcc" "CMakeFiles/ftxui_starter.dir/src/my_lib.cpp.o.d"
+  "" "ftxui_starter" "gcc" "CMakeFiles/ftxui_starter.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Tvmth/OneDrive/FTXUI Project
+# Install script for directory: /home/mintori/Documents/FTXUI-PBL2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ftxui_starter")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -34,15 +39,21 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Tvmth/OneDrive/FTXUI Project/build/ftxui_build/cmake_install.cmake")
+  include("/home/mintori/Documents/FTXUI-PBL2/build/ftxui_build/cmake_install.cmake")
 
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/home/mintori/Documents/FTXUI-PBL2/build/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -56,8 +67,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Tvmth/OneDrive/FTXUI Project/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/mintori/Documents/FTXUI-PBL2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
