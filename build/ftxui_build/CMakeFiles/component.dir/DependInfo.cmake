4cd297e6133d6ec3d48550d9fc39bc1640041f6a
Tran Van Minh Tri <tvmthtr@gmail.com>
2024-12-30 22:35:57 +0700

    update

---
+++ build/ftxui_build/CMakeFiles/component.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/animation.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/animation.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/animation.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/button.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/button.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/button.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/catch_event.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/catch_event.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/catch_event.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/checkbox.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/checkbox.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/checkbox.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/collapsible.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/collapsible.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/collapsible.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/component.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/component.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/component.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/component_options.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/component_options.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/component_options.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/container.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/container.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/container.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/dropdown.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/dropdown.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/dropdown.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/event.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/event.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/event.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/hoverable.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/hoverable.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/hoverable.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/input.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/input.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/input.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/loop.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/loop.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/loop.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/maybe.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/maybe.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/maybe.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/menu.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/menu.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/menu.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/modal.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/modal.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/modal.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/radiobox.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/radiobox.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/radiobox.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/renderer.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/renderer.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/renderer.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/resizable_split.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/resizable_split.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/resizable_split.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/screen_interactive.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/screen_interactive.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/screen_interactive.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/slider.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/slider.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/slider.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/terminal_input_parser.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/terminal_input_parser.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/terminal_input_parser.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/util.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/util.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/util.cpp.o.d"
+  "/home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/component/window.cpp" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/window.cpp.o" "gcc" "ftxui_build/CMakeFiles/component.dir/src/ftxui/component/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/ftxui_build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
@@ -0,0 +1,4 @@
+ftxui_build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
+ /home/mintori/Documents/FTXUI-PBL2/ftxui/src/ftxui/dom/flexbox_config.cpp \
+ /usr/include/stdc-predef.h \
+ /home/mintori/Documents/FTXUI-PBL2/ftxui/include/ftxui/dom/flexbox_config.hpp
